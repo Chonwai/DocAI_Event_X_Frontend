a854ab62642269535461bab9739a0502ccb416bd
聪 <505810824@qq.com>
2024-10-31 18:09:35 +0800

    update home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,15 @@ export interface EventForm {
 export default function Home() {
     const router = useRouter();
     const [formDatas, setFormDatas] = useState<EventForm[]>([]);
+    const title = `HKU Information Day 2024 in Macau 香港大學本科入學資訊日2024（澳門）`
+    const description = `
+    Date 日期：9 November 2024 (Saturday)​ 2024年11月9日（星期六）<br/>
+    Time 時間： 9:00am-6:00pm<br/>
+    Venue 地點：Chan Sui Ki Perpetual Help College, 28, Estrada da Vitoria, Macau<br/>
+    澳門得勝馬路廿八號陳瑞祺永援中學<br/>
+    <br/>
+    Organized By 主辦單位: 香港大學 The University of Hong Kong<br/>
+    Co-Organized By 承辦單位: 澳門聯校科學展覽青年協會 Macao Joint School Science Exhibition Youth Association<br/>`
 
     useEffect(() => {
         fetchAllFormData();
@@ -52,8 +61,10 @@ export default function Home() {
                         >
                             <p className="font-semibold text-2xl">{data?.json_schema?.title}</p>
 
-                            <p className='mt-4 font-semibold text-xl'>{data?.name}</p>
-                            <div dangerouslySetInnerHTML={{ __html: data?.description }} />
+                            <p className='mt-4 font-semibold text-xl'>{title}</p>
+                            <div dangerouslySetInnerHTML={{
+                                __html: description
+                            }} />
                             <div className='mt-2 flex justify-end'>
                                 <button className='flex items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition'
                                     onClick={() => {
