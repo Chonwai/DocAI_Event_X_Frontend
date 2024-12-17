c284d7c6696d145324f588cccfe1777be09f36d8
Chonwai <chonwaiun@gmail.com>
2024-12-17 21:34:12 +0800

    Add: Add Chakra UI dependencies and update package.json

--- src/app/forms/[id]/page.tsx
+++ src/app/forms/[id]/page.tsx
@@ -4,7 +4,12 @@ import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import { QRCodeCanvas } from 'qrcode.react';
 import { useEffect, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/theme-utils';
+import Form from '@rjsf/chakra-ui';
+import validator from '@rjsf/validator-ajv8';
+
+const theme = extendTheme({});
 
 export interface FormDetail {
     id: string;
@@ -23,16 +28,9 @@ export interface FormDetail {
 }
 
 export default function FormDetail() {
-    const {
-        register,
-        handleSubmit,
-        control,
-        formState: { errors }
-    } = useForm();
     const router = useRouter();
     const params = useParams();
     const [qrCode, setQrCode] = useState(null);
-    const [formId, setFormId] = useState(null);
     const [formData, setFormData] = useState<FormDetail>();
     const [submissionData, setSubmissionData] = useState<any>();
     const [loading, setLoading] = useState<boolean>(true);
@@ -43,14 +41,12 @@ export default function FormDetail() {
     }, [params]);
 
     const fetchFormDataById = async () => {
-        console.log('form id', params['id']);
         setLoading(true);
         const formId = params['id'] || process.env.NEXT_PUBLIC_FORM_ID;
         const response = await axios.get(
             `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/forms/${formId}`
         );
         setLoading(false);
-        console.log('response.data', response.data);
         if (response.data.success) {
             setFormData(response.data.form);
         } else {
@@ -58,15 +54,14 @@ export default function FormDetail() {
         }
     };
 
-    const onSubmit = async (data: any) => {
-        console.log('submit form data', data);
-        setSubmissionData(data);
+    const onSubmit = async ({ formData: submitData }: any) => {
+        setSubmissionData(submitData);
         setSubmitting(true);
         try {
             const formId = params['id'] || process.env.NEXT_PUBLIC_FORM_ID;
             const response = await axios.post(
                 `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/form_submissions`,
-                { form_submission: { form_id: formId, submission_data: data } }
+                { form_submission: { form_id: formId, submission_data: submitData } }
             );
             setSubmitting(false);
             setQrCode(response.data.form_submission.qrcode_id);
@@ -85,150 +80,36 @@ export default function FormDetail() {
         return <div className="text-center mt-10">Loading...</div>;
     }
 
-    const requiredFields = formData.json_schema.required || [];
-
     return (
-        <div className="container mx-auto p-4 max-w-3xl">
-            <img src="../bg.jpeg" alt="背景圖片" />
-            {!qrCode && (
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-4">
-                    {formData.display_order.map((key) => {
-                        const fieldSchema = formData.json_schema.properties[key];
-                        const uiSchema = formData.ui_schema[key];
-                        const isRequired = requiredFields.includes(key);
-                        // console.log('key', fieldSchema);
-                        if (!fieldSchema) return <div key={key}></div>;
-
-                        return (
-                            <div key={key} className="flex flex-col">
-                                <label className="font-medium mb-1">
-                                    {fieldSchema.title}
-                                    {isRequired && <span className="text-red-500"> *</span>}
-                                </label>
-                                {fieldSchema.type === 'string' &&
-                                uiSchema?.['ui:widget'] === 'select' ? (
-                                    <Controller
-                                        name={key}
-                                        control={control}
-                                        defaultValue={formData.form_data[key]}
-                                        rules={{
-                                            required: isRequired
-                                                ? `${fieldSchema.title} 是必填項`
-                                                : false
-                                        }}
-                                        render={({ field }) => (
-                                            <div className="flex flex-col">
-                                                {fieldSchema.enum.map((item: string) => (
-                                                    <label key={item} className="flex items-center">
-                                                        <input
-                                                            type="radio"
-                                                            value={item}
-                                                            checked={field.value === item}
-                                                            onChange={() => field.onChange(item)}
-                                                            className="mr-2"
-                                                        />
-                                                        {item}
-                                                    </label>
-                                                ))}
-                                            </div>
-                                        )}
-                                    />
-                                ) : fieldSchema.type === 'array' &&
-                                  uiSchema?.['ui:widget'] === 'checkboxes' ? (
-                                    <Controller
-                                        name={key}
-                                        control={control}
-                                        defaultValue={formData.form_data[key]}
-                                        rules={{
-                                            required: isRequired
-                                                ? fieldSchema.items.enum.length > 0
-                                                    ? `${fieldSchema.title} 至少選擇一項`
-                                                    : false
-                                                : false
-                                        }}
-                                        render={({ field }) => (
-                                            <div className="flex flex-col">
-                                                {fieldSchema.items.enum.map((item: string) => (
-                                                    <label key={item} className="flex items-center">
-                                                        <input
-                                                            type="checkbox"
-                                                            value={item}
-                                                            checked={field.value.includes(item)}
-                                                            onChange={(e) => {
-                                                                const newValue = e.target.checked
-                                                                    ? [...field.value, item]
-                                                                    : field.value.filter(
-                                                                          (v: string) => v !== item
-                                                                      );
-                                                                field.onChange(newValue);
-                                                            }}
-                                                            className="mr-2"
-                                                        />
-                                                        {item}
-                                                    </label>
-                                                ))}
-                                            </div>
-                                        )}
-                                    />
-                                ) : (
-                                    <Controller
-                                        name={key}
-                                        control={control}
-                                        defaultValue={formData.form_data[key]}
-                                        rules={{
-                                            required: isRequired
-                                                ? `${fieldSchema.title} 是必填項`
-                                                : false
-                                        }}
-                                        render={({ field }) => (
-                                            <input
-                                                {...field}
-                                                placeholder={fieldSchema.title}
-                                                className="border rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                            />
-                                        )}
-                                    />
-                                )}
-                                {errors[key] && (
-                                    <span className="text-red-500 text-sm mt-1">
-                                        {String(errors[key]?.message)}
-                                    </span>
-                                )}
-                            </div>
-                        );
-                    })}
-                    <div className="flex justify-end">
-                        <button
-                            type="submit"
-                            disabled={submitting}
-                            className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition ${
-                                submitting ? 'opacity-50 cursor-not-allowed' : ''
-                            }`}
-                        >
-                            {submitting ? '提交中...' : '提交'}
-                        </button>
+        <ChakraProvider theme={theme}>
+            <div className="container mx-auto p-4 max-w-3xl">
+                <img src="../bg.jpeg" alt="背景圖片" />
+                {!qrCode && (
+                    <Form
+                        schema={formData.json_schema}
+                        uiSchema={formData.ui_schema}
+                        formData={formData.form_data}
+                        validator={validator}
+                        onSubmit={onSubmit}
+                        disabled={submitting}
+                    />
+                )}
+                {qrCode && (
+                    <div className="mt-8 text-center flex flex-col items-center justify-center">
+                        <h2 className="text-4xl font-extrabold text-green-600 mb-4">報名成功!</h2>
+                        <p className="text-xl font-semibold mb-2">您很快將收到確認電子郵件。</p>
+                        <p className="text-xl font-semibold mb-2">
+                            Registration successful! You will receive a confirmation email shortly.
+                        </p>
+                        <h3 className="text-xl font-semibold mb-2">您的電子門票 QR Code</h3>
+                        <QRCodeCanvas value={qrCode} size={256} />
+                        <p className="mt-2">請活動當天顯示二維碼入場。</p>
+                        <p className="mt-2">
+                            Please show this QR code for entry on the day of the event.
+                        </p>
                     </div>
-                </form>
-            )}
-
-            {qrCode && (
-                <div className="mt-8 text-center flex flex-col items-center justify-center">
-                    <h2 className="text-4xl font-extrabold text-green-600 mb-4">報名成功!</h2>
-                    <p className="text-xl font-semibold mb-2">您很快將收到確認電子郵件。</p>
-                    <p className="text-xl font-semibold mb-2">
-                        Registration successful! You will receive a confirmation email shortly.
-                    </p>
-                    {/* <h3 className="text-xl font-semibold mb-2">您的電子門票 QR Code</h3>
-                    <QRCodeCanvas value={qrCode} size={256} />
-                    <p className="mt-2">請活動當天顯示此二維碼入場。</p>
-                    <p className="mt-2">
-                        Please show this QR code for entry on the day of the event.
-                    </p> */}
-                    {/* <p className="mt-2">
-                        姓名 Name: {submissionData?.lastName} {submissionData?.firstName}
-                    </p> */}
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </ChakraProvider>
     );
 }
